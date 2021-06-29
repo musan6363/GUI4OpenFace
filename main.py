6abbd8cc8091a9717b5f96cb79708cd0148e575a
musan6363 <msn151336+prog@gmail.com>
2021-06-29 11:03:05 +0900

    実行コマンドをsubprocess.runの引数として渡してOpenFaceの実行

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 import subprocess
 
+# CMD = "~/openface/OpenFace-OpenFace_2.2.0/build/bin/FaceLandmarkVidMulti"  # 実行するコマンドのパス, Not found
+CMD = "/Users/mrkm-cmc/openface/OpenFace-OpenFace_2.2.0/build/bin/FaceLandmarkVidMulti"  # 実行するコマンドのパス
+
+# inputvideo = "~/openface/OpenFace-OpenFace_2.2.0/samples/multi_face.avi"  # Not found
+inputvideo = "/Users/mrkm-cmc/openface/OpenFace-OpenFace_2.2.0/samples/multi_face.avi"  # 【要修正】直接指定せずwith文等で走査させる
+# outdir = "~/cmc/210629/test"  # 【要修正】引数で親ディレクトリを与えて，その中に動画ごとにフォルダを作る．
+outdir = "/Users/mrkm-cmc/GoogleDrive/NU/cmc/210629/test"  # 【要修正】引数で親ディレクトリを与えて，その中に動画ごとにフォルダを作る．
+
 
 class TextWidget(Widget):
     text = StringProperty()    # プロパティの追加
@@ -14,9 +22,10 @@ class TextWidget(Widget):
         self.text = ''
 
     def buttonClicked(self):        # ボタンをクリック時
-        result = subprocess.run("~/openface/OpenFace-OpenFace_2.2.0/build/bin/FaceLandmarkVidMulti -f ~/openface/OpenFace-OpenFace_2.2.0/samples/multi_face.avi -out_dir ~/cmc/210628/test",
-                                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        self.text = "Running..."
+        result = subprocess.run([CMD, "-f", inputvideo, "-out_dir", outdir], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         print(result.returncode)
+        print(result.stdout)
         if result.returncode == 0:
             self.text = "DONE"
         else:
