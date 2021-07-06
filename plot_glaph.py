f68bcae83c3777735ccfa1cc3514cf7d315582e8
musan6363 <msn151336+prog@gmail.com>
2021-07-06 16:16:49 +0900

    線形補間のグラフ出力と，メモリリークの防止

--- plot_glaph.py
+++ plot_glaph.py
@@ -2,13 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import interpolate
 import numpy as np
+from multiprocessing import Pool
 
-'''
-グラフに表示する要素を追加するときは，追加箇所1,2を変更すれば良い．
-AUは"AUxx_r"が各AUの強度(0〜5)，"AUxx_c"が0 absent, 1 presentを示す．
-'''
-
-# 追加箇所1
 # columnsに検出の対象にする値を追加．face_id, timestamp, successは必須．
 # Openfaceの出力CSVは列によって先頭に空白文字を含むことに注意．ミスが起きやすいので，定数定義で対応を取ると良い．
 # 【改善案】csvcolumns.pyで全ての列名とその番号を保持，呼び出しプログラム側で引数を変えるだけでできるようにしたい．
@@ -20,61 +15,54 @@ AU06_R = 3
 AU12_R = 4
 
 
-class PlotGlaph:
-    def __init__(self, csv: str, save_dir: str, save_name: str):
-        self.csv = csv
-        self.save_dir = save_dir
-        self.save_name = save_name
+def PlotGlaph(csv: str, save_dir: str, save_name: str):
+    df = pd.read_csv(csv)
+    data = df.loc[:, columns]
 
-        self.extract_csv()
+    target_face_id = get_mode_face_id(data)  # 最も多く登場するface_idを取得する
 
-    def extract_csv(self):
-        df = pd.read_csv(self.csv)
-        data = df.loc[:, columns]
+    # 最も多く登場する顔のうち，処理に成功しているものを抽出
+    target = []
+    for line in data.values:
+        if line[FACE_ID] == target_face_id and line[SUCCESS] == 1:
+            target.append(line)
 
-        faceid_cnt = []
-        for _ in range(data[columns[FACE_ID]].max() + 1):
-            faceid_cnt.append(0)
+    df = pd.DataFrame(target, columns=columns)
 
-        for line in data.values:
-            faceid_cnt[int(line[FACE_ID])] += int(line[SUCCESS])
+    # npdata[0]:timestamp, npdata[1]:AU06, npdata[2]:AU12
+    npdata = df[[columns[TIMESTAMP], columns[AU06_R], columns[AU12_R]]].values.T
+    x_latent = np.linspace(min(npdata[0]), max(npdata[0]), 100)  # 保管用のX軸データ
 
-        target_face_id = faceid_cnt.index(max(faceid_cnt))
+    liner_au06 = interpolate.interp1d(x=npdata[0], y=npdata[1])
+    liner_au12 = interpolate.interp1d(x=npdata[0], y=npdata[2])
 
-        target = []
-        for line in data.values:
-            # 最も多く登場する顔のうち，処理に成功しているものを抽出
-            if line[FACE_ID] == target_face_id and line[SUCCESS] == 1:
-                target.append(line)
+    p = Pool(1)
+    p.map(plot, [[x_latent, liner_au06, liner_au12, save_dir, save_name]])
+    p.close()
 
-        self.df = pd.DataFrame(target, columns=columns)
-        self.plot()
 
-    def plot(self):
-        # 追加箇所2
-        # 追加したデータ分，plot命令を追加する．
-        # colorはグラフ上で見分けるために変えたほうが良い．
-        # ax = self.df.plot.scatter(x=columns[TIMESTAMP], y=columns[AU06_R], label='AU06', color='red')
-        # ax = self.df.plot.scatter(x=columns[TIMESTAMP], y=columns[AU12_R], label='AU12', color='blue', ax=ax)
-        # tmp[0]:timestamp, tmp[1]:AU06, tmp[2]:AU12
-        tmp = self.df[[columns[TIMESTAMP], columns[AU06_R], columns[AU12_R]]].values.T
-        x_latent = np.linspace(min(tmp[0]), max(tmp[0]), 100)
-        print(tmp[0])
-        fitted_curve = interpolate.interp1d(x=tmp[0], y=tmp[1])
-        plt.scatter(tmp[0], tmp[1], label="observed")
-        plt.plot(x_latent, fitted_curve(x_latent), c="red", label="fitted")
-        plt.grid()
-        plt.legend()
-        plt.show()
+def get_mode_face_id(data):
+    faceid_cnt = []
+    for _ in range(data[columns[FACE_ID]].max() + 1):
+        faceid_cnt.append(0)
 
-        # ax = self.df.plot(x=columns[TIMESTAMP], y=columns[AU06_R], label='AU06', color='red')
-        # ax = self.df.plot(x=columns[TIMESTAMP], y=columns[AU12_R], label='AU12', color='blue', ax=ax)
+    for line in data.values:
+        faceid_cnt[int(line[FACE_ID])] += int(line[SUCCESS])
 
-        # plt.grid()
-        # plt.legend()
-        # plt.show()
-        plt.savefig(self.save_dir + "/" + self.save_name + ".png")
-        plt.close()
+    return faceid_cnt.index(max(faceid_cnt))
+
+
+def plot(args):
+    x_latent, liner_au06, liner_au12, save_dir, save_name = args
+
+    fig = plt.figure()
+    plt.plot(x_latent, liner_au06(x_latent), c="red", label="AU06")
+    plt.plot(x_latent, liner_au12(x_latent), c="blue", label="AU12")
+    plt.grid()
+    plt.legend()
+    # plt.show()
+
+    fig.savefig(save_dir + "/" + save_name + ".png")
 
 
 if __name__ == '__main__':
